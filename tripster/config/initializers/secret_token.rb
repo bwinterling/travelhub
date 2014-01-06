fee47fee00509b5c4644ec196350ddaf9c6bed8a
Brian Winterling <bwinterling@yahoo.com>
2014-01-06 14:05:54 -0700

    initial project setup

---
+++ tripster/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tripster::Application.config.secret_key_base = '210431141372105ea5e9b4f967e6dab5c1b0df1e913330b7a6e6d1b1813f34ac5ef75f73f11c530939e3375959bf7e3c2ad733cce6d0b0de30bbd8b55574e08d'
