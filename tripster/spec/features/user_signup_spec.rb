5e769b3a221aa998cee25929fcd077d2e821c1d3
Katrina Engelsted <katrina@engelsted.co>
2014-01-06 16:59:53 -0700

    making rspec test actually fail and not give error

--- tripster/spec/features/user_signup_spec.rb
+++ tripster/spec/features/user_signup_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
   describe 'User signup' do
-    visit new_user_path
-    fill_in 'Email', :with => "trippytripster@example.com"
-    fill_in 'Name', :with => "The Trippiest"
-    # avatar test
-    click_on 'Sign Up'
-    expect(page).to have_content('Connect') 
+    it 'signup' do
+      page.visit new_user_path
+      fill_in 'Email', :with => "trippytripster@example.com"
+      fill_in 'Name',   :with => "The Trippiest"
+      # avatar test
+      click_on 'Sign Up'
+      expect(page).to have_content('Connect') 
+    end
   end
