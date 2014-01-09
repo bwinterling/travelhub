024f8ea5aa090400738e5848e829a47e9e5fd5d5
Katrina Engelsted <katrina@engelsted.co>
2014-01-09 11:31:17 -0700

    editing routes

--- tripster/config/routes.rb
+++ tripster/config/routes.rb
@@ -1,18 +1,19 @@
 Tripster::Application.routes.draw do
 
-  resources :users
-  resources :sessions
-  match "/logout", to: "sessions#destroy", as: "logout", via: "delete"
-  resources :trips
   root "users#index"
+
+  resources :users
+  resources :trips
+
+  match "/logout", to: "sessions#destroy", as: "logout", via: "delete"
+  match 'auth/twitter/callback', to: 'sessions#create', via: "get"
+
+  match 'auth/failure', to: redirect('/'), via: "get"
+
+  match "/dashboard", to: "trips#dashboard", as: "dashboard", via: "get"
 
   namespace :feeds do
     get '/photos/connect', to: 'photos#connect'
     get '/photos/callback', to: 'photos#callback'
   end
-
-  match "/dashboard", to: "trips#dashboard", as: "dashboard", via: "get"
-  match 'auth/twitter/callback', to: 'sessions#create', via: "get"
-  match 'auth/failure', to: redirect('/'), via: "get"
-
 end
