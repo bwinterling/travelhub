2fd0dbc1c2bee73e4ef13eb8ebd8527c6b2a6677
Rolen Le <rolentle@gmail.com>
2014-01-12 12:39:01 -0700

    pushed tweets/messages into the trip model

--- tripster/app/controllers/application_controller.rb
+++ tripster/app/controllers/application_controller.rb
@@ -1,20 +1,17 @@
 class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
- 
-  before_action :current_user
 
-  # def tweets
-  #   @tweets = MessagesAPI.new(current_user).client.user_timeline
-  # end
+  before_action :current_user
 
 private
 
   def current_user
     if session[:user_id]
-      @current_user ||= User.find(session[:user_id]) 
-    end  
+      @current_user ||= User.find(session[:user_id])
+    end
   end
+
   helper_method :current_user
 
 end
--- tripster/app/models/messages_api.rb
+++ tripster/app/models/messages_api.rb
@@ -3,8 +3,8 @@ class MessagesAPI < ActiveRecord::Base
 
   def initialize(current_user)
     @current_user = current_user
-    @client = Twitter::Client.new 
-    @client.consumer_key    = TWITTER_CONSUMER_KEY 
+    @client = Twitter::Client.new
+    @client.consumer_key    = TWITTER_CONSUMER_KEY
     @client.consumer_secret = TWITTER_CONSUMER_SECRET
     @client.oauth_token     = @current_user.access_token
     @client.oauth_token_secret = @current_user.access_secret
