a8cf3ef3e34f0f851bce2ae1495be0e6918dc976
Brian Winterling <bwinterling@yahoo.com>
2014-01-13 16:58:52 -0700

    added method to user model to provide possessive name

--- tripster/app/models/user.rb
+++ tripster/app/models/user.rb
@@ -4,6 +4,10 @@ class User < ActiveRecord::Base
   has_many :feed_sources
   has_many :photos
 
+  def possessive_name
+    name.ends_with?("s") ? name + "'" : name + "'s"
+  end
+
   def self.from_omniauth(auth)
     where(auth.slice("provider","uid")).first || create_from_omniauth(auth)
   end
