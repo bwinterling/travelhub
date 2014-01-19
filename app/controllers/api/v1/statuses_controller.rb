b41f6571d87a94dcf1c98aa7794bb583037b97a4
Rolen Le <rolentle@gmail.com>
2014-01-18 18:04:45 -0700

    fixed api status bug

--- app/controllers/api/v1/statuses_controller.rb
+++ app/controllers/api/v1/statuses_controller.rb
@@ -1,12 +1,14 @@
 class Api::V1::StatusesController < ApplicationController
+   skip_before_filter :verify_authenticity_token
   def index
     @statuses = Status.all
     render json: @statuses, status: 200
   end
 
   def create
+    puts "Asasshdfjalsdfhlasdf \n  #{params} \n"
     status_params = params[:status]
-    user = User.find_by(status_params[:user_id])
+    user = User.find(status_params[:user_id].to_i)
     @status = user.statuses.new
     @status.text = status_params[:text]
     @status.sent_at = status_params[:sent_at]
