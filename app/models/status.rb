788058f3b9238755b1619b16e4f6760fec47a53d
Brian Winterling <bwinterling@yahoo.com>
2014-01-22 10:12:50 -0700

    photos are now added as timeline dates

--- app/models/status.rb
+++ app/models/status.rb
@@ -3,4 +3,20 @@ class Status < ActiveRecord::Base
   validates :text, :presence => true
   validates :sent_at, :presence => true, uniqueness: { scope: :user_id,
 						       message: "a user can only send on status at a time" }
+
+  def timeline_hash
+    {
+      startDate: sent_at.strftime("%Y,%m,%d"),
+      endDate: sent_at.+(1.day).strftime("%Y,%m,%d"),
+      headline:"Twitter",
+      text:"<p>I like hash tags!</p>",
+      asset: {
+        media: standard_url,
+        thumbnail: thumbnail_url,
+        credit: user.name,
+        caption: caption
+      }
+    }
+  end
+
 end
