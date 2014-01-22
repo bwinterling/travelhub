77399b1ea2387f7d0dea0b9f21d2a8b83bdad1e8
Brian Winterling <bwinterling@yahoo.com>
2014-01-22 13:49:08 -0700

    change to checkin

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -3,6 +3,7 @@ class Checkin < ActiveRecord::Base
 
   def google_map_url
     "http://maps.google.com/?q=#{venue_latitude},#{venue_longitude}"
+    "https://www.google.com/url?q=http://maps.google.com/maps%3Fq%3Dchicago%26hl%3Den%26sll%3D41.874961,-87.619054%26sspn%3D0.159263,0.351906%26t%3Dt%26hnear%3DChicago,%2BCook,%2BIllinois%26z%3D11&usd=2&usg=ALhdy28o-smFGlAQ-_jlleH2EmcQ1ZqfIg"
   end
 
   def timeline_hash
@@ -12,7 +13,7 @@ class Checkin < ActiveRecord::Base
       headline:"Foursquare",
       text:"<p>Checked in at #{venue_name}</p>",
       asset: {
-        media: "https://foursquare.com/rolentle/checkin/52d6f46e498ee3a629fa2e04",
+        media: google_map_url,
         credit: user.name,
         caption: shout
       }
