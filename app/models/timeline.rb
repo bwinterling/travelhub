7be28365da4a9d2501e205cc07c20187ac1d7095
Brian Winterling <bwinterling@yahoo.com>
2014-01-22 10:01:00 -0700

    mock data

--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -7,52 +7,65 @@ class Timeline
   end
 
   def return_json
-  {
-    timeline:
     {
-      headline: "Timeline for #{trip.name}",
-      type:"default",
-      text:"<p>#{trip.description}</p>",
+      timeline:
+      {
+        headline: "Timeline for #{trip.name}",
+        type:"default",
+        text:"<p>#{trip.description}</p>",
+        asset: {
+          media:"http://www.jaunted.com/files/6193/the_griswalds.jpg",
+          credit:"jaunted.com",
+          caption:"Hi Aunt Edna!"
+        },
+        date: [
+    {
+      startDate:"2011,12,10",
+      endDate:"2011,12,11",
+      headline:"Hey yall, check this out!",
+      text:"<p>This is an HTML paragraph</p>",
       asset: {
-        media:"http://www.jaunted.com/files/6193/the_griswalds.jpg",
-        credit:"jaunted.com",
-        caption:"Hi Aunt Edna!"
-      },
-      date: [
-	{
-	  startDate:"2011,12,10",
-	  endDate:"2011,12,11",
-	  headline:"Headline Goes Here",
-	  text:"<p>Body text goes here, some HTML is OK</p>",
-	  tag:"This is Optional",
-	  asset: {
-	    media:"http://twitter.com/ArjunaSoriano/status/164181156147900416",
-	    thumbnail:"optional-32x32px.jpg",
-	    credit:"Credit Name Goes Here",
-	    caption:"Caption text goes here"
-	  }
-	}
-      ],
-      era: [
-	{
-	  startDate:"2011,1,10",
-	  endDate:"2011,1,11",
-	  headline:"Headline Goes Here",
-	  tag:"This is Optional"
-	}
-
-      ],
-      chart: [
-	{
-	  startDate:"2011,12,10",
-	  endDate:"2011,12,11",
-	  headline:"Headline Goes Here",
-	  value:"28"
-	}
-
-      ]
-
+        media: "http://failfun.com/wp-content/uploads/mini-bike-jump.jpg",
+        # thumbnail:"optional-32x32px.jpg",
+        credit:"failfun.com",
+        caption:"Caption Weee"
+      }
+    },
+    {
+      startDate:"2012,03,10",
+      endDate:"2012,03,11",
+      headline:"Headline Goes Here",
+      text:"<p>Body text goes here, some HTML is OK</p>",
+      # tag:"This is Optional",
+      asset: {
+        media:"http://twitter.com/ArjunaSoriano/status/164181156147900416",
+        thumbnail:"optional-32x32px.jpg",
+        credit:"Credit Name Goes Here",
+        caption:"Caption text goes here"
+      }
     }
-  }.to_json
+        ]
+        # ,
+   #      era: [
+  	# {
+  	#   startDate:"2011,1,10",
+  	#   endDate:"2011,1,11",
+  	#   headline:"Headline Goes Here",
+  	#   tag:"This is Optional"
+  	# }
+
+   #      ],
+   #      chart: [
+  	# {
+  	#   startDate:"2011,12,10",
+  	#   endDate:"2011,12,11",
+  	#   headline:"Headline Goes Here",
+  	#   value:"28"
+  	# }
+
+   #      ]
+
+      }
+    }.to_json
   end
 end
